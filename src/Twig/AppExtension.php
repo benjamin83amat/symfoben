be41fcda8643e6fd1d5bacebc27937cd0221c732
Benji <beniamin.tolan@gmail.com>
2018-06-26 18:36:30 +0200

    Boostrap popper WEBPACK

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BenjiT7
+ * Date: 26/06/2018
+ * Time: 14:52
+ */
+
+namespace App\Twig;
+
+
+use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension implements GlobalsInterface
+{
+
+
+    /**
+     * @var string
+     */
+    private $locale;
+
+    public function __construct(string $locale)
+    {
+        $this->locale = $locale;
+    }
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('price', [$this, 'priceFilter'])
+        ];
+    }
+
+    public function getGlobals()
+    {
+        return [
+            'locale' => $this->locale
+        ];
+    }
+
+
+
+    public function priceFilter($number)
+    {
+        return '$'.number_format($number, 2, '.', ',');
+    }
+
+}
